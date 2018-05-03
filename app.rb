dee53993c065b4b4f8fe0b00a700ebfe667f5a42
maiya-22 <maiya_o@yahoo.com>
2018-05-03 13:13:02 -0400

    saving progress

--- app.rb
+++ app.rb
@@ -3,6 +3,7 @@ require 'sinatra/activerecord'
 require 'rake'
 require 'pg'
 require 'faker'
+require 'json'
 
 # require all of the models:
 Dir[settings.root + "/models/*.rb"].each {|file| require file}
@@ -20,26 +21,40 @@ get '/test' do
 
     # WHY DON'T THE FOLLOWING LINES WORK?
     # WHAT DO I NEED TO FIX?
-    # @post = Post.find(1)
-    # @tags = @post.tags
+    @post = Post.find(1)
+    @tags = @post.tags
 
-    # p "@posts:"
-    # p @posts
+    p "@tags:"
+    p @tags
   
-    # @tag = Tag.find(1)
-    # @posts = @tag.posts
+    @tag = Tag.find(1)
+    @posts = @tag.posts
 
-    # p "@tags:"
-    # p @tags
+    p "@posts:"
+    p @posts
 
 
 
     #THESE LINES ARE WORKING, BUT IT SEEMS LIKE A HACK
     #WHAT IS THE BETTER WAY OF DOING THE QUERY? 
-    @post = Post.find(1)
-    @tags = Post_tag.where(post_id: @post.id)
-    p @tags
+    # @post = Post.find(1)
+    # @tags = Post_tag.where(post_id: @post.id)
+    # p @tags
 
 
     "testing join table queries"
+end
+
+
+# retrieve all tags
+get "/tags" do
+    p " ***  ALL TAGS **  /n"
+    p Tag.all.to_json
+end
+
+# retrieve all tags on a certain post:
+get "/post/:post_id/tags" do
+    # p " ***  ALL TAGS ON A CERTAIN POST **  /n"
+    # @post = post.find(params[:post_id])
+    # @tags = @post.tags
 end
