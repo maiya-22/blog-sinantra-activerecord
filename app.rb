c2ffbd5f7e12d54470770edac4ee699f8706f4c7
maiya-22 <maiya_o@yahoo.com>
2018-05-03 12:48:12 -0400

    did another query in the get '/test' route

--- app.rb
+++ app.rb
@@ -17,17 +17,29 @@ get '/' do
 end
 
 get '/test' do 
-    @post = Post.find(1)
-    @tags = @post.tags
 
-    p "@posts:"
-    p @posts
+    # WHY DON'T THE FOLLOWING LINES WORK?
+    # WHAT DO I NEED TO FIX?
+    # @post = Post.find(1)
+    # @tags = @post.tags
+
+    # p "@posts:"
+    # p @posts
   
-    @tag = Tag.find(1)
-    @posts = @tag.posts
+    # @tag = Tag.find(1)
+    # @posts = @tag.posts
 
-    p "@tags:"
+    # p "@tags:"
+    # p @tags
+
+
+
+    #THESE LINES ARE WORKING, BUT IT SEEMS LIKE A HACK
+    #WHAT IS THE BETTER WAY OF DOING THE QUERY? 
+    @post = Post.find(1)
+    @tags = Post_tag.where(post_id: @post.id)
     p @tags
-   
+
+
     "testing join table queries"
 end
